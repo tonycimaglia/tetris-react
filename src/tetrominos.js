ce043c57f763b2da02e40ef93416ab65cfb804a1
tonycimaglia <tonycimaglia@gmail.com>
2020-01-14 21:31:42 -0500

    finished game tiles and wrote function to randomize tiles for game.

--- src/tetrominos.js
+++ src/tetrominos.js
@@ -57,3 +57,9 @@ export const TETROMINOS = {
     color: '227, 78, 78',
   },
 }
+
+export const randomTetromino = () => {
+  const tetrominos = 'IJLOSTZ';
+  const randomTile = tetrominos[Math.floor(Math.random() * tetrominos.length)];
+    return TETROMINOS[randomTile];
+}
