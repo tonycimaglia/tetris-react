83da4d109fbe7bfd9a711ef249e419d52c4b0823
tonycimaglia <tonycimaglia@gmail.com>
2020-01-17 20:54:13 -0500

    added stling for tetris wrapper components.

--- src/components/styled-components/StyledTetris.js
+++ src/components/styled-components/StyledTetris.js
@@ -1,1 +1,23 @@
-import styled from 'styled-components'
+import styled from 'styled-components';
+
+import bgImage from '../../img/bg.png';
+
+export const StyledTetrisWrapper = styled.div`
+  width: 100vw;
+  height 100vh;
+  background: url(${bgImage}) #000;
+  background-size: cover;
+  overflow: hidden;
+`
+
+export const StyledTetris = styled.div`
+  display: flex;
+  align-items: flex-start;
+  padding: 40px;
+  margin: 0 auto;
+  max-width: 900px;
+
+  aside {
+    
+  }
+`
