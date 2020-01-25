cef71a7848614ca4ec895402910223caddd19fc4
tonycimaglia <tonycimaglia@gmail.com>
2020-01-24 21:12:58 -0500

    created styled strart button and added it to the ui. removed yellow from game grid.

--- src/components/StartButton.js
+++ src/components/StartButton.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import {StartButtonStyled} from './styled-components/StartButtonStyled'
 
 const StartButton = ({ callback }) => (
-  <div>
+  <StartButtonStyled>
     StartGame
-  </div>
+  </StartButtonStyled>
 )
 
 export default StartButton;
